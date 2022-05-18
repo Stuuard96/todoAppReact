3410c20e26984d61789122636d259f402c6e2cf3
Stuard Quiroz <stuuard96@gmail.com>
2022-05-18 14:53:31 -0500

    Creacion de los hooks y el guardar datos

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,11 +1,99 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
+import { todoReducer } from "./hooks/todoReducer";
+import { useForm } from "./hooks/useForm";
 import "./index.css";
 
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
 export const TodoApp = () => {
+  const [todos, dispatch] = useReducer(todoReducer, [], init);
+
+  const [{ description }, handleInputChange, reset] = useForm({
+    description: "",
+  });
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleDelete = (todoId) => {
+    const action = {
+      type: "delete",
+      payload: todoId,
+    };
+
+    dispatch(action);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (description.trim().length <= 1) {
+      return;
+    }
+
+    const newTodo = {
+      id: new Date().getTime(),
+      desc: description,
+      done: false,
+    };
+
+    const action = {
+      type: "add",
+      payload: newTodo,
+    };
+
+    dispatch(action);
+    reset();
+  };
+
   return (
     <div>
-      <h1>Todo App</h1>
+      <h1>TodoApp ({todos.length})</h1>
       <hr />
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group list-group-flush">
+            {todos.map((todo, i) => (
+              <li key={todo.id} className="list-group-item">
+                <p className="text-center">
+                  {i + 1}. {todo.desc}
+                </p>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => handleDelete(todo.id)}
+                >
+                  Borrar
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="col-5">
+          <h4>Agregar TODO</h4>
+          <hr />
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="description"
+              className="form-control"
+              placeholder="Aprender ..."
+              autoComplete="off"
+              value={description}
+              onChange={handleInputChange}
+            ></input>
+            <button
+              type="submit"
+              className="btn btn-outline-primary mt-2 btn-block"
+            >
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
