f326ebdfdf7a9f49e0b86d155489ab211a6ba7e1
Stuard Quiroz <stuuard96@gmail.com>
2022-05-18 16:10:48 -0500

    Culminamos app

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useReducer } from "react";
+import { TodoAdd } from "./components/TodoAdd";
 import { TodoList } from "./components/TodoList";
 import { todoReducer } from "./hooks/todoReducer";
-import { useForm } from "./hooks/useForm";
 import "./index.css";
 
 const init = () => {
@@ -10,10 +10,6 @@ const init = () => {
 
 export const TodoApp = () => {
   const [todos, dispatch] = useReducer(todoReducer, [], init);
-
-  const [{ description }, handleInputChange, reset] = useForm({
-    description: "",
-  });
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
@@ -35,26 +31,11 @@ export const TodoApp = () => {
     });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (description.trim().length <= 1) {
-      return;
-    }
-
-    const newTodo = {
-      id: new Date().getTime(),
-      desc: description,
-      done: false,
-    };
-
-    const action = {
+  const handleAddTodo = (newTodo) => {
+    dispatch({
       type: "add",
       payload: newTodo,
-    };
-
-    dispatch(action);
-    reset();
+    });
   };
 
   return (
@@ -71,25 +52,7 @@ export const TodoApp = () => {
         </div>
 
         <div className="col-5">
-          <h4>Agregar TODO</h4>
-          <hr />
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              name="description"
-              className="form-control"
-              placeholder="Aprender ..."
-              autoComplete="off"
-              value={description}
-              onChange={handleInputChange}
-            ></input>
-            <button
-              type="submit"
-              className="btn btn-outline-primary mt-2 btn-block"
-            >
-              Agregar
-            </button>
-          </form>
+          <TodoAdd handleAddTodo={handleAddTodo} />
         </div>
       </div>
     </div>
